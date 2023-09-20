57d81380fd237097384e4807cffa185b8d478c87
Sibin Grasic <sibin.grasic@oblak.studio>
2023-09-20 20:31:01 +0200

    style: Impemented shellcheck recommendations

--- redis/scripts/setup-drupal-settings.sh
+++ redis/scripts/setup-drupal-settings.sh
@@ -11,12 +11,12 @@ if ( ddev debug configyaml 2>/dev/null | grep 'disable_settings_management:\s*tr
   exit 0
 fi
 
-cp redis/scripts/settings.ddev.redis.php $DDEV_APPROOT/$DDEV_DOCROOT/sites/default/
+cp redis/scripts/settings.ddev.redis.php "$DDEV_APPROOT/$DDEV_DOCROOT/sites/default/"
 
 SETTINGS_FILE_NAME="${DDEV_APPROOT}/${DDEV_DOCROOT}/sites/default/settings.php"
 echo "Settings file name: ${SETTINGS_FILE_NAME}"
-grep -qF 'settings.ddev.redis.php' $SETTINGS_FILE_NAME || echo "
+grep -qF 'settings.ddev.redis.php' "$SETTINGS_FILE_NAME" || echo "
 // Include settings required for Redis cache.
 if ((file_exists(__DIR__ . '/settings.ddev.redis.php') && getenv('IS_DDEV_PROJECT') == 'true')) {
   include __DIR__ . '/settings.ddev.redis.php';
-}" >> $SETTINGS_FILE_NAME
+}" >> "$SETTINGS_FILE_NAME"
